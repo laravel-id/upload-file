34ddcc5a0edfbd74eb64b08fccabdc40528527ab
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-11-29 20:08:18 +0700

    Add repository link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,31 +43,8 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
+                        <li><a href="https://github.com/laravel-id/upload-file"><i class="fa fa-github fa-fw"></i> Repository</a></li>
                         <li><a href="{{ route('file.index') }}">Files</a></li>
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
                     </ul>
                 </div>
             </div>
